dbc926f22e0dfc103af9843c89064f529d0e55eb
samuelboko <samuel.bokobza@gmail.com>
2014-08-29 17:25:02 +0200

    nouvelle version

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+(function(){
+
+
+	$('.grid').on('click',function () {
+		$(this).toggleClass("click-change");
+	});	
+	$('.proposer').on('click',function () {
+		$('.proposer').toggleClass("click-change");
+	});	
+	$('.choisir').on('click',function () {
+		$('.choisir').toggleClass("click-change");
+	});
+
+
+}());
+
+
+
+
+
+/*$('li').on('mouseover',function(){
+	 $(".ui-page-theme-a .ui-btn").css("background-color","red");
+});*/
+/*	$('#panel').on('click', function(){
+		$( "#panel" ).panel( "close" );
+	})*/
+	
+
+/*	$('.grille-b').on('click', function(){
+		$(".grille-b").addClass("clicks");
+	});*/
+/*$('#map').on('click', function(){
+	alert();
+});
+
+$("#map3").geolocate({
+	lat: "#lat3",
+	lng: "#lng3",
+	address: ["#address3"]
+});*/
+
